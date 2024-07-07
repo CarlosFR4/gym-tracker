06922621d1571e5e25b6111ffcedff63647d894a
CarlosFR4 <cfr4123@gmail.com>
2024-07-06 22:20:35 -0600

    Update model for exercise, add Id.

--- src/entities/exercises/Exercise.js
+++ src/entities/exercises/Exercise.js
@@ -5,12 +5,15 @@
 export default class Exercise {
   /**
    * Creates an exercise.
+   * @constructor
+   * @param {number} id - The id
    * @param {string} name - The name of the exercise.
    * @param {string|null} image - The URL of the image for the exercise.
    * @param {string} bodyPart - The body part that the exercise targets.
    * @param {string} category - The category of the exercise.
    */
-  constructor(name, image, bodyPart, category) {
+  constructor(id, name, image, bodyPart, category) {
+    this.id = id
     this.name = name
     this.image = image
     this.bodyPart = bodyPart
